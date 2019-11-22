dd987134a539d9034d6c6073aa1fafa0a9966961
mark <voschezang>
2019-11-22 20:23:12 +0100

    rm dummy heartbeat

--- lab/upscaling/worker/Worker.py
+++ lab/upscaling/worker/Worker.py
@@ -1,21 +1,14 @@
-import time
-
 from lab.util.graph import Graph
 from lab.master.WorkerInterface import WorkerInterface
-from lab.util import message
-# from lab.upscaling.worker.Algorithm import Algorithm
 from lab.upscaling.worker import Algorithm
 
 
 class Worker(WorkerInterface):
     def run(self):
-        for _ in range(2):
-            time.sleep(0.)
-            self.send_message_to_master(message.write_alive(self.worker_id))
-
         graph = Graph()
         graph.load_from_file(self.graph)
         diff = Algorithm.DegreeDistrubution().run(graph)
         # TODO send confirmation to master, then ask for permission to write
-        filename = f'{self.graph}-par-{self.worker_id}.txt'
+        prefix, ext = self.graph.split('.')
+        filename = f'{prefix}-par-{self.worker_id}{ext}'
         diff.save_to_file(filename)
