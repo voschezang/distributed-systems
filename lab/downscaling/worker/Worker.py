12c68ffd6ee04a0516b694b81510a57c2c41bab9
Eric <eric.zonneveld0@gmail.com>
2019-11-23 18:38:02 +0100

    work in progress

--- lab/downscaling/worker/Worker.py
+++ lab/downscaling/worker/Worker.py
@@ -1,9 +1,41 @@
-from lab.proof_of_concept import *
+from lab.util.graph import *
 import numpy as np
+from lab.util import message
+import time
+from lab.master.WorkerInterface import WorkerInterface
 
-class Worker():
-    def __init__(self, graph):
-        self.graph = graph
+
+class Worker(WorkerInterface):
+    def run(self):
+            """
+            Runs the worker
+            """
+            # self.send_debug_message(self.graph)
+            self.g = graph_from_file(self.graph)
+            self.send_debug_message(str(self.g))
+
+            self.min_vertex = self.meta_data_of_all_workers[self.worker_id].min_vertex
+            self.max_vertex = self.meta_data_of_all_workers[self.worker_id].max_vertex
+
+            # self.send_debug_message(str(self.meta_data_of_all_workers[self.worker_id].min_vertex))
+            scale = 0.5 # TODO: niet hardcoded maar arg
+            cur_vertex = self.g.vertices[np.random.choice(len(self.g.vertices))]
+            vertices = {cur_vertex}
+            edges = set()
+            goal_size = int(scale * self.g.n_edges) # TODO: moet van totale aantal edges zijn, niet van zijn eigen.
+
+            while len(edges) < 44000:
+                new_vertex = self.g.edges[cur_vertex][np.random.choice(len(self.g.edges[cur_vertex]))]
+
+
+            #while True:
+            #    time.sleep(1)
+            #    self.send_message_to_master(message.write_alive(self.worker_id))
+
+    # def __init__(self, graph, worker_id: int, master_host: str, master_port: int, graph_path: str):
+        # super().__init__(worker_id, master_host, master_port, graph_path)
+        # self.g = graph_from_file(graph_path)
+        # self.g.load_from_file(graph)
 
     def downscale(self, scale=0.5, method="rw"):
         if method == "rw":
@@ -32,11 +64,11 @@ class Worker():
         print(new_graph)
 
 
-if __name__ == '__main__':
-    g = Graph()
+#if __name__ == '__main__':
+    #g = Graph()
     #g.load_from_file('facebook_combined.txt')
     #g.save_as_pickle()
-    g = g.load_pickle()
-    worker = Worker(g)
-    worker.downscale()
+    #g = g.load_pickle()
+    #worker = Worker(g)
+    #worker.downscale()
     #print(g)
--- lab/downscaling/worker/__init__.py
+++ lab/downscaling/worker/__init__.py
@@ -6,6 +6,7 @@ from lab.util.validation import (
     assert_host,
     assert_path)
 from lab.downscaling.worker.DummyWorker import DummyWorker
+from lab.downscaling.worker.Worker import Worker
 
 
 def run(worker_id: int, master_host: str, master_port: int, graph_path: str):
@@ -18,7 +19,7 @@ def run(worker_id: int, master_host: str, master_port: int, graph_path: str):
     :param graph_path: The path to the graph to downscale
     """
 
-    worker = DummyWorker(worker_id, master_host, master_port, graph_path)
+    worker = Worker(worker_id, master_host, master_port, graph_path)
 
 
 def main():
