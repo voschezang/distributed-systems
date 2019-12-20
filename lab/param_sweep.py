a18c81468d0d5d8d328f5147c64bd409c89d6903
mark <voschezang>
2019-12-20 22:48:34 +0100

    test n_workers

--- lab/param_sweep.py
+++ lab/param_sweep.py
@@ -4,57 +4,60 @@ import pandas as pd
 from time import time, sleep
 import networkx as nx
 
-from lab.util.graph import Graph
 from lab.master.Upscaler import Upscaler
 from lab.util.metrics import summarize
 from lab.master.Master import Master
-from lab.upscaling.worker.Algorithm import GScalerAlgorithm
-# from lab.master.__init__ import main
-
 
 dir = 'data/WS'
-master_func = Upscaler
-# for dirpath, dirnames, filenames in os.walk(dir):
-# for f in filenames:
+extra_metrics = True
 results = []
-for _ in range(1):
-    n_workers = 2
 
-    # generate graph
-    path = 'tmp/generated_graph.txt'
-    G = nx.watts_strogatz_graph(n=1000, k=2, p=0.1)
-    nx.readwrite.edgelist.write_edgelist(G, path, data=False)
-    # path = 'data/facebook_combined.txt'
-    result = summarize(path)
-    result['name'] = 'Original'
-    result['dt'] = 0
-    result['scale'] = 1
-    results.append(result)
+# generate graph
+print('Generate graph')
+path = 'tmp/generated_graph.txt'
+G = nx.watts_strogatz_graph(n=10000, k=2, p=0.3)
+nx.readwrite.edgelist.write_edgelist(G, path, data=False)
+print('Summarize')
+result = summarize(path, extra_metrics=extra_metrics)
+result['name'] = 'Original'
+result['dt'] = 0
+result['scale'] = 1
+results.append(result)
 
-    for scale in [2, 10]:
-        # for scale in [0.5]:
-        # DegreeDistribution
+for n_workers in [4, 8]:
+    for scale in [2]:
+        print(f'\n\nn_workers: {n_workers}, scale: {scale}\n')
         output_file = 'tmp/scaled_graph.txt'
         t0 = time()
-        Upscaler(
-            worker_hostnames=np.arange(n_workers), graph_path=path,
-            worker_script='lab/upscaling/worker/__init__.py', split_graph=True,
-            output_file=output_file, scale=scale, method='DegreeDistribution')
-
-        # Master(
-        #     worker_hostnames=np.arange(n_workers), graph_path=path,
-        #     worker_script='lab/downscaling/worker/__init__.py', split_graph=True,
-        #     output_file=output_file, scale=scale, method='random_edge')
+        if scale < 1:
+            # downscaling
+            name = f'Random edge ({scale}x)'
+            Master(
+                worker_hostnames=np.arange(n_workers), graph_path=path,
+                worker_script='lab/downscaling/worker/__init__.py', split_graph=True,
+                output_file=output_file, scale=scale, method='random_edge')
+        else:
+            # upscaling
+            name = f'Degree Distribution ({scale}x, n: {n_workers})'
+            print('scale', scale)
+            Upscaler(
+                worker_hostnames=np.arange(n_workers), graph_path=path,
+                worker_script='lab/upscaling/worker/__init__.py', split_graph=True,
+                output_file=output_file, scale=scale, method='DegreeDistribution')
 
         dt = time() - t0
-        result = summarize(output_file)
-        result['name'] = f'Degree Distribution ({scale})'
-        result['dt'] = dt
+        print('Summarize result')
+        result = summarize(output_file, extra_metrics=extra_metrics)
+        result['name'] = name
+        result['dt'] = np.round(dt, 3)
         result['scale'] = scale
         results.append(result)
+        sleep(0.1)
 
 result = pd.DataFrame(results)
 with open(f'param_sweep_result.pkl', 'wb') as f:
-    pickle.dump(results, f)
+    pickle.dump(result, f)
 
+print(result)
+print('\n\n')
 print(result.transpose())
